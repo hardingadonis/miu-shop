91e8c7e9ff6f832953a1439b113f784e7b7613f0
Minh Vương <hardingadonis@gmail.com>
2023-11-19 22:56:16 +0700

    Add LogoutServlet
    - Just use doGet method

---
+++ src/main/java/io/hardingadonis/miu/controller/web/LogoutServlet.java
@@ -0,0 +1,23 @@
+package io.hardingadonis.miu.controller.web;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class LogoutServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html; charset=UTF-8");
+
+        HttpSession session = request.getSession(false);
+
+        if (session != null) {
+            session.invalidate();
+        }
+
+        response.sendRedirect("home");
+    }
+}
