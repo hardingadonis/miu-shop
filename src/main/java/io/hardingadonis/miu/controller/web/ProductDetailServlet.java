923d3c443ead5f39e1ca6c4b9f89855fb4c7c88b
Minh Vương <hardingadonis@gmail.com>
2023-11-20 16:51:15 +0700

    Add ProductDeltailServlet
    - Just use doGet method
    - If id not found or invalid, redirct to home
    - Else, forward to jsp file

---
+++ src/main/java/io/hardingadonis/miu/controller/web/ProductDetailServlet.java
@@ -0,0 +1,36 @@
+package io.hardingadonis.miu.controller.web;
+
+import io.hardingadonis.miu.model.*;
+import io.hardingadonis.miu.services.*;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class ProductDetailServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html; charset=UTF-8");
+
+        String IDStr = request.getParameter("id");
+
+        try {
+            int ID = Integer.parseInt(IDStr);
+
+            Product product = Singleton.productDAO.get(ID);
+
+            if (product != null) {
+                request.setAttribute("product", product);
+                request.getRequestDispatcher("/view/web/product.jsp").forward(request, response);
+
+                return;
+            }
+        } catch (NumberFormatException ex) {
+            System.err.println(ex.getMessage());
+        }
+
+        response.sendRedirect("home");
+    }
+}
