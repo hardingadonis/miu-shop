b4b362f7a32fa4bf55f31b43fc6d9212c6cabb4e
HoangVersion <huyhoangero@gmail.com>
2023-11-24 21:26:59 +0700

    Register-form Finish-1.2

--- src/main/java/io/hardingadonis/miu/controller/web/RegisterServlet.java
+++ src/main/java/io/hardingadonis/miu/controller/web/RegisterServlet.java
@@ -32,6 +32,34 @@ public class RegisterServlet extends HttpServlet {
         String sex = request.getParameter("sex");
         String email = request.getParameter("email");
         String password = request.getParameter("password");
-    }
 
+        try {
+            // Check if email already exists
+            User existingUser = Singleton.userDAO.get(email);
+            if (existingUser != null) {
+                response.sendRedirect("register?emailExist=true");
+            } else {
+                // Create a new user object
+                User newUser = new User();
+                newUser.setFullName(name);
+                newUser.setBirthYear(yob);
+                newUser.setGender(UserGender.create(sex));
+                newUser.setEmail(email);
+                newUser.setHashedPassword(Hash.SHA256(password));
+                newUser.setStatus(UserStatus.ACTIVATE);
+
+                // Insert the new user using the UserDAO
+                Singleton.userDAO.insert(newUser);
+
+                response.sendRedirect("login?registerSuccess=true");
+
+            }
+        } catch (Exception e) {
+            out.println("Fail!");
+            e.printStackTrace(out);
+            out.println("Error: " + e.getMessage());
+        }
+
+        out.close();
+    }
 }
