abcfde96c0f285b3049bd65060315869c5af69b0
Minh Vương <34091632+hardingadonis@users.noreply.github.com>
2023-11-29 18:01:08 +0700

    Update searchHandler.js

--- src/main/webapp/assets/js/web/searchHandler.js
+++ src/main/webapp/assets/js/web/searchHandler.js
@@ -19,25 +19,3 @@ function formatCurrencyVND(amount) {
 
     return formatter.format(amount);
 }
-
-function updateProductHeight() {
-    let products = document.querySelectorAll('.row .product-detail');
-    let maxHeight = 0;
-
-    products.forEach(function (product) {
-        product.style.height = 'auto';
-        let productHeight = product.offsetHeight;
-        maxHeight = Math.max(maxHeight, productHeight);
-    });
-
-    products.forEach(function (product) {
-        product.style.height = maxHeight + 'px';
-    });
-}
-
-window.addEventListener('load', function () {
-    setTotalProductQuantityToCart();
-    updateProductHeight();
-});
-window.addEventListener('resize', updateProductHeight);
-}
