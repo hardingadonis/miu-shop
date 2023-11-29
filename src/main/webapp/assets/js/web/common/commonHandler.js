0ad5d9c4b32a88634be6255454ab00268c572254
Minh Vương <hardingadonis@gmail.com>
2023-11-29 17:40:33 +0700

    Use encode and decode for json string in cookie

--- src/main/webapp/assets/js/web/common/commonHandler.js
+++ src/main/webapp/assets/js/web/common/commonHandler.js
@@ -3,7 +3,7 @@ function getCartCookie() {
     for (let i = 0; i < cookies.length; i++) {
         let cookie = cookies[i].trim();
         if (cookie.startsWith('cart=')) {
-            return JSON.parse(cookie.substring(5));
+            return JSON.parse(decodeURIComponent(cookie.substring(5)));
         }
     }
 
