cb1ce5fa0094f70d56aa86bce2321c820677c5aa
Minh Vương <hardingadonis@gmail.com>
2023-12-05 23:24:11 +0700

    Add Checkout Success Servlet class

---
+++ src/main/java/io/hardingadonis/miu/controller/web/CheckoutSuccessServlet.java
@@ -0,0 +1,25 @@
+package io.hardingadonis.miu.controller.web;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import javax.servlet.http.*;
+
+@WebServlet(name = "CheckoutSuccessServlet", urlPatterns = {"/checkout-success"})
+public class CheckoutSuccessServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html; charset=UTF-8");
+
+        HttpSession session = request.getSession();
+
+        if ((session.getAttribute("user") == null) || (session.getAttribute("order") == null) || (session.getAttribute("cart_cookie") == null)) {
+            response.sendRedirect("home");
+        }
+
+        request.getRequestDispatcher("/view/web/checkout-success.jsp").forward(request, response);
+    }
+}
