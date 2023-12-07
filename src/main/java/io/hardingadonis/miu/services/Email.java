0cf45f0edd7df677755cf76d5ca88c6b24de40ee
Minh Vương <hardingadonis@gmail.com>
2023-12-07 17:07:38 +0700

    Add ``sendVerifyEmail`` method in Email service and its implementer

--- src/main/java/io/hardingadonis/miu/services/Email.java
+++ src/main/java/io/hardingadonis/miu/services/Email.java
@@ -1,8 +1,11 @@
 package io.hardingadonis.miu.services;
 
 import io.hardingadonis.miu.model.*;
+import javax.servlet.http.*;
 
 public interface Email {
 
     public void sendWelcomeEmail(User user);
+    
+    public void sendVerifyEmail(User user, String code, HttpServletRequest request);
 }
